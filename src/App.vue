d61c963cb7eb9f07c3edabab636673f3958e88e6
jklwlong <jklwlong@163.com>
2017-07-27 19:54:25 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <Top></Top>
+    <Left></Left>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+import Top from './components/common/Top.vue'
+import Left from './components/common/Left.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Top,
+    Left
+  }
+}
+</script>
